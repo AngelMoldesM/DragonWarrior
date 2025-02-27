94bbb1b6182af64ecf3752dfc632921b2da48f98
AngelMoldesM <a22angelmm@iessanclemente.net>
2025-02-27 18:00:49 +0100

    Cambios varios en scripts

--- Assets/Scripts/GoalController.cs
+++ Assets/Scripts/GoalController.cs
@@ -1,32 +1,71 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalController : MonoBehaviour
 {
-private AudioManager audioManager;
-   [SerializeField] public TextMeshProUGUI winMessageText;  // Referencia al texto que muestra el mensaje
-    [SerializeField] public TextMeshProUGUI coinCountText;   // Referencia al texto que muestra las monedas
-    [SerializeField] public PlayerController playerController; // Referencia al jugador
+   private AudioManager audioManager;
 
-    [SerializeField] CoinController coinController;
+    [SerializeField] private TextMeshProUGUI winMessageText;
+    [SerializeField] private TextMeshProUGUI coinCountText; 
+    [SerializeField] private TextMeshProUGUI restartMessageText;
+    [SerializeField] private PlayerController playerController; 
+    [SerializeField] private CoinController coinController; 
+
+    private bool gameWon = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !gameWon)
         {
+            gameWon = true; 
+
+ 
             audioManager = FindAnyObjectByType<AudioManager>();
             audioManager.PlayVictorySound();
+
+   
             winMessageText.gameObject.SetActive(true);
             winMessageText.text = "¡Has ganado!";
 
-
+     
             coinCountText.gameObject.SetActive(true);
             coinCountText.text = "Monedas: " + coinController.GetCoinCount();
 
+   
+            restartMessageText.gameObject.SetActive(true);
+            restartMessageText.text = "Presiona 'R' para reiniciar o 'Esc' para salir.";
 
-            playerController.enabled = true;
+    
+            playerController.enabled = false;
 
+ 
+            DisableAllEnemies();
+        }
+    }
+
+    private void DisableAllEnemies()
+    {
+        // Buscar y desactivar todos los enemigos tipo MeleeEnemy
+        MeleeEnemy[] enemies = FindObjectsByType<MeleeEnemy>(FindObjectsSortMode.None);
+        foreach (MeleeEnemy enemy in enemies)
+        {
+            enemy.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (gameWon)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reiniciar nivel
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit(); // Salir del juego
+            }
         }
     }
 }
